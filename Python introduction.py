c64bddc1b1911309b9a0df5b6f299f44baf84f29
EdsonHernandezGonzalez <61528875+EdsonHernandezGonzalez@users.noreply.github.com>
2020-03-02 23:19:22 -0600

    Add files via upload

---
+++ Python introduction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 13:25:01 2020
+
+@author: edson
+"""
+Name="Golden"
+Last_name="boy"
+Age="18 years"
+Height="1.80mts"
+ID="93111914"
+Crime="Stole"
+
+print('Last Name: {:20}'.format(Last_name))
+print('Name: {:20}'.format(Name))
+print('Age: {:20}'.format(Age))
+print('Height: {:20}'.format(Height))
+print('ID: {:20}'.format(ID))
+print('Crime: {:20}'.format(Crime))
